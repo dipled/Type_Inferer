fdb2b5fe03851814193b121ce4224cf474218ea3
helena <helenavargastannuri@gmail.com>
2024-06-04 08:28:54 -0300

    Não sei se é assim que parseia booleano

--- Lex.hs
+++ Lex.hs
@@ -1,11 +1,7 @@
 import Text.Parsec
-import Type
-
-import qualified Text.Parsec.Token as L
 import Text.Parsec.Language (emptyDef)
+import Text.Parsec.Token qualified as L
 import Type
-
-
 
 -- tiContext g i = if l /= [] then t else error ("Undefined: " ++ i ++ "\n")
 --    where
@@ -22,58 +18,81 @@ import Type
 --                         (t, s)  <- tiExpr (g/+/[i:>:b]) e
 --                         return (apply s (b --> t), s)
 
--- --- Examples ---
--- ex1 = Lam "f" (Lam "x" (App (Var "f") (Var "x")))
--- ex2 = Lam "x" (App (Var "x") (Var "x"))
--- ex3 = Lam "g" (Lam "f" (Lam "x" (App (Var "g") (App (Var "f") (Var "x")))))
--- ex4 = Lam "x" (Lam "x" (Var "x"))
--- ex5 = Lam "w" (Lam "y" (Lam "x" (App (Var "y") (App (App (Var "w") (Var "y")) (Var "x")))))
--- ex6 = Lam "x" (Lam "y" (Lam "w" (Lam "u" (App (App (Var "x") (Var "w")) (App (App (Var "y") (Var "w")) (Var "u"))))))
+--- Examples ---
+ex1 = Lam "f" (Lam "x" (App (Var "f") (Var "x")))
+
+ex2 = Lam "x" (App (Var "x") (Var "x"))
+
+ex3 = Lam "g" (Lam "f" (Lam "x" (App (Var "g") (App (Var "f") (Var "x")))))
+
+ex4 = Lam "x" (Lam "x" (Var "x"))
+
+ex5 = Lam "w" (Lam "y" (Lam "x" (App (Var "y") (App (App (Var "w") (Var "y")) (Var "x")))))
+
+ex6 = Lam "x" (Lam "y" (Lam "w" (Lam "u" (App (App (Var "x") (Var "w")) (App (App (Var "y") (Var "w")) (Var "u"))))))
 
 -- infer e = runTI (tiExpr [] e)
 
 -- -------- Lexical ---------------
 
-lingDef = emptyDef
-          { L.commentStart = "{-"
-           ,L.commentEnd   = "-}"
-           ,L.commentLine  = "--"
-           ,L.identStart   = letter
-           ,L.identLetter  = letter
-          }  
+lingDef =
+  emptyDef
+    { L.commentStart = "{-",
+      L.commentEnd = "-}",
+      L.commentLine = "--",
+      L.identStart = letter,
+      L.identLetter = letter,
+      L.reservedNames = ["True", "False"]
+    }
 
 lexical = L.makeTokenParser lingDef
 
-symbol     = L.symbol lexical
-parens     = L.parens lexical
-identifier = L.identifier lexical  
+reserved = L.reserved lexical
+
+literalInt = L.integer lexical
+
+symbol = L.symbol lexical
+
+parens = L.parens lexical
+
+identifier = L.identifier lexical
 
 -- --------- Parser -----------------
--- parseExpr = runParser expr [] "lambda-calculus"
+parseExpr = runParser expr [] "lambda-calculus"
 
--- expr :: Parsec String u Expr
--- expr = chainl1 parseNonApp $ return $ App
+expr :: Parsec String u Expr
+expr = chainl1 parseNonApp $ return $ App
 
--- var = do {i <- identifier; return (Var i)}
+var = do i <- identifier; return (Var i)
 
--- lamAbs = do symbol "\\"
---             i <- identifier
---             symbol "."
---             e <- expr
---             return (Lam i e)
+literal = 
+    do i <- literalInt; return $ Lit $ LitInt i
+    <|> do reserved "True"; return $ Lit $ LitBool True
+    <|> do reserved "False"; return $ Lit $ LitBool False
 
--- parseNonApp = parens expr      -- (E)
---               <|> lamAbs       -- \x.E
---               <|> var          -- x
+lamAbs = 
+  do
+    symbol "\\"
+    i <- identifier
+    symbol "."
+    e <- expr
+    return (Lam i e)
 
--- ----------------------------------------
+parseNonApp =
+    parens expr -- (E)
+    <|> lamAbs -- \x.E
+    <|> var -- x
+    <|> literal
+
+----------------------------------------
 -- parseLambda s = case parseExpr s of
 --                      Left er -> print er
 --                      Right e -> (print e >> print (infer e))
+parseLambda s = print $ parseExpr s
 
--- main = do putStr "Lambda:"
---           e <- getLine
---           parseLambda e
+main = do
+  putStr "Lambda:"
+  e <- getLine
+  parseLambda e
 
-
---unexpected emite mensagem de erro
+-- unexpected emite mensagem de erro
