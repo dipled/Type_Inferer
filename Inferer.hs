49404f6a15da732b171257865e372a4cecc686aa
helena <helenavargastannuri@gmail.com>
2024-06-07 12:20:54 -0300

    added inferer stuff from criscris

--- Inferer.hs
+++ Inferer.hs
@@ -1,2 +1,29 @@
 import Lex
+import Type
+import Data.List(nub, intersect, union)
 
+
+tiContext g i = if l /= [] then t else error ("Undefined: " ++ i ++ "\n")
+   where
+      l = dropWhile (\(i' :>: _) -> i /= i' ) g
+      (_ :>: t) = head l
+
+tiExpr g (Var i) = return (tiContext g i, [])
+tiExpr g (App e e') = do (t, s1) <- tiExpr g e
+                         (t', s2) <- tiExpr (apply s1 g) e'
+                         b <- freshVar
+                         let s3 = unify (apply s2 t) (t' --> b)
+                         return (apply s3 b, s3 @@ s2 @@ s1)
+tiExpr g (Lam i e) = do b <- freshVar
+                        (t, s)  <- tiExpr (g/+/[i:>:b]) e
+                        return (apply s (b --> t), s)
+
+--- Examples ---
+ex1 = Lam "f" (Lam "x" (App (Var "f") (Var "x")))
+ex2 = Lam "x" (App (Var "x") (Var "x"))
+ex3 = Lam "g" (Lam "f" (Lam "x" (App (Var "g") (App (Var "f") (Var "x")))))
+ex4 = Lam "x" (Lam "x" (Var "x"))
+ex5 = Lam "w" (Lam "y" (Lam "x" (App (Var "y") (App (App (Var "w") (Var "y")) (Var "x")))))
+ex6 = Lam "x" (Lam "y" (Lam "w" (Lam "u" (App (App (Var "x") (Var "w")) (App (App (Var "y") (Var "w")) (Var "u"))))))
+
+infer e = runTI (tiExpr [] e)
--- Type.hs
+++ Type.hs
@@ -32,9 +32,86 @@ data SimpleType = TVar Id
     | TCon String
     | TApp SimpleType SimpleType
     | TGen Int
-    deriving (Eq, Show)
+    deriving Eq
 
 data Assump = Id :>: SimpleType deriving (Eq, Show)
 
 
 iniCont = ["(,)" :>: (TArr (TGen 0) (TArr (TGen 1) (TApp (TApp (TCon "(,)") (TGen 0)) (TGen 1)))), "True" :>: (TCon "Bool"), "False" :>: (TCon "Bool")]
+
+
+instance Show SimpleType where
+   show (TVar i) = i
+   show (TArr (TVar i) t) = i++" -> "++show t   
+   show (TArr t t') = "("++show t++")"++"->"++show t'          
+--------------------------
+instance Functor TI where
+   fmap f (TI m) = TI (\e -> let (a, e') = m e in (f a, e'))
+
+instance Applicative TI where
+   pure a = TI (\e -> (a, e))
+   TI fs <*> TI vs = TI (\e -> let (f, e') = fs e; (a, e'') = vs e' in (f a, e''))              
+
+instance Monad TI where 
+--   return x = TI (\e -> (x, e))
+   TI m >>= f  = TI (\e -> let (a, e') = m e; TI fa = f a in fa e')
+
+freshVar :: TI SimpleType
+freshVar = TI (\e -> let v = "t"++show e in (TVar v, e+1))
+
+runTI (TI m) = let (t, _) = m 0 in t 
+----------------------------
+t --> t' = TArr t t' 
+
+infixr 4 @@
+(@@)       :: Subst -> Subst -> Subst
+s1 @@ s2    = [ (u, apply s1 t) | (u,t) <- s2 ] ++ s1
+
+dom = map (\(i:>:_)->i)
+
+as /+/ as' = as' ++ filter compl as
+   where
+     is = dom as'
+     compl (i:>:_) = notElem i is
+---------------------------- 
+class Subs t where
+  apply :: Subst -> t -> t
+  tv    :: t -> [Id]
+
+instance Subs SimpleType where
+  apply s (TVar u)  =   
+                    case lookup u s of
+                       Just t  -> t
+                       Nothing -> TVar u
+  apply s (TArr l r) =  (TArr (apply s l) (apply s r))
+
+
+  tv (TVar u)  = [u]
+  tv (TArr l r) = tv l `union` tv r
+
+
+instance Subs a => Subs [a] where
+  apply s     = map (apply s)
+  tv          = nub . concat . map tv
+  
+instance Subs Assump where
+  apply s (i:>:t) = i:>:apply s t
+  tv (i:>:t) = tv t
+  
+------------------------------------
+varBind :: Id -> SimpleType -> Maybe Subst
+varBind u t | t == TVar u   = Just []
+            | u `elem` tv t = Nothing
+            | otherwise     = Just [(u, t)]
+      
+mgu (TArr l r,  TArr l' r') = do s1 <- mgu (l,l')
+                                 s2 <- mgu ((apply s1 r) ,  (apply s1 r'))
+                                 return (s2 @@ s1)
+mgu (t,        TVar u   )   =  varBind u t
+mgu (TVar u,   t        )   =  varBind u t
+
+unify t t' =  case mgu (t,t') of
+    Nothing -> error ("\ntrying to unify:\n" ++ (show t) ++ "\nand\n" ++
+                      (show t')++"\n")
+    Just s  -> s
+ 
